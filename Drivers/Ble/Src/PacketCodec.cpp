218201523fa552e201dfdd9d841a703a59ed8060
jwj1874 <jonwoojin@gmail.com>
2026-03-09 13:41:22 +0900

    fix : Data Processor fix complete

--- Drivers/Ble/Src/PacketCodec.cpp
+++ Drivers/Ble/Src/PacketCodec.cpp
@@ -35,27 +35,31 @@ void * PacketCodec::Encoding(void *argument){
 //여기서부터 다시 수정
 void *PacketCodec::Decoding(void *argument){
     uint8_t *buf = static_cast<uint8_t *>(argument);
-    data.mode_data = buf[1];
-    data.moter_x = buf[2];
-    data.moter_y = buf[3];
-    data.gripper = buf[4];
+    data.mode_data = 0;
+    data.mode_data = static_cast<uint16_t>(buf[1]);
+
+    data.gripper = 0;
+    data.gripper = static_cast<uint16_t>(buf[2]);
+
+    data.moter_x = 0;
+    data.moter_x = static_cast<uint16_t>(buf[3]);
+    data.moter_x |= static_cast<uint16_t>(buf[4] << 8);
+
+    data.moter_y = 0;
+    data.moter_y = static_cast<uint16_t>(buf[5]);
+    data.moter_y |= static_cast<uint16_t>(buf[6] << 8);
+
     data.servo_top = 0;
-    data.servo_top = (uint32_t)(buf[5]);
-    data.servo_top |= (uint32_t)(buf[6] << 8);
-    data.servo_top |= (uint32_t)(buf[7] << 16);
-    data.servo_top |= (uint32_t)(buf[8] << 24);
-    
+    data.servo_top = static_cast<uint16_t>(buf[7]);
+    data.servo_top |= static_cast<uint16_t>(buf[8] << 8);
+
     data.servo_mid = 0;
-    data.servo_mid = (uint32_t)(buf[9]);
-    data.servo_mid |= (uint32_t)(buf[10] << 8);
-    data.servo_mid |= (uint32_t)(buf[11] << 16);
-    data.servo_mid |= (uint32_t)(buf[12] << 24);
+    data.servo_mid = static_cast<uint16_t>(buf[9]);
+    data.servo_mid |= static_cast<uint16_t>(buf[10] << 8);
 
     data.servo_bot = 0;
-    data.servo_bot = (uint32_t)(buf[13]);
-    data.servo_bot |= (uint32_t)(buf[14] << 8);
-    data.servo_bot |= (uint32_t)(buf[15] << 16);
-    data.servo_bot |= (uint32_t)(buf[16] << 24);
+    data.servo_bot = static_cast<uint16_t>(buf[11]);
+    data.servo_bot |= static_cast<uint16_t>(buf[12] << 8);
 
     return &data;
 }
