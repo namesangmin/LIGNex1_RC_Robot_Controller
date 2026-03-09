9e25823ff9263d60457af8b6e121c2d45eb6b4e3
jwj1874 <jonwoojin@gmail.com>
2026-03-09 14:36:04 +0900

    feat : Create Rx Task in Controller

--- App/Tasks/BluetoothRxTask/Src/BluetoothRxTask.cpp
+++ App/Tasks/BluetoothRxTask/Src/BluetoothRxTask.cpp
@@ -1,10 +1,15 @@
 #include "BluetoothRxTask.hpp"
-
+#include "IRx.hpp"
+#include "BleRx.hpp"
+#include "DataConfig.hpp"
 #include "cmsis_os2.h"
 
 extern "C"
 void BluetoothRxTaskHandler(void *argument){
 
+    IRx *Rx = new BleRx;
+    Init_type init = {&huart1, &RxQhandleHandle, NULL, NULL};
+    Rx->Init(&init);
     for(;;){
         osDelay(1);
     }
